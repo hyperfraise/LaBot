7e3a807c16b4ced193a9b3fcad429b8b5a526c36
hyperfraise <damien.menigaux@polytechnique.edu>
2021-10-21 13:51:46 +0200

    make it compatible with ubuntu

--- scripts/decompile.sh
+++ scripts/decompile.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-export DofusInvoker="/Applications/Dofus.app/Contents/Data/Dofus.app/Contents/Resources/DofusInvoker.swf"
+export DofusInvoker="/home/veesion/.config/Ankama/Dofus/DofusInvoker.swf"
 export selectclass='com.ankamagames.dofus.BuildInfos,com.ankamagames.dofus.network.++,com.ankamagames.jerakine.network.++'
 export config='parallelSpeedUp=0'
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ..
 
-/Applications/FFDec.app/Contents/Resources/ffdec.sh \
+bash /usr/share/java/ffdec/ffdec.sh \
   -config "$config" \
     -selectclass "$selectclass" \
       -export script \
